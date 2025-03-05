6f69292e1e67a3aa82d9de0dc917c73ce4b93a7d
Dokesh Kumar Gadtia <110685614+thedk201003@users.noreply.github.com>
2025-03-05 16:40:54 +0530

    DatabaseConfig changed

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -5,10 +5,8 @@ import java.io.InputStream;
 import java.util.Properties;
 
 class DatabaseConfig {
-
     static Properties prop = new Properties();
     static {
-
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream input = classLoader.getResourceAsStream("application.properties");
 
@@ -19,12 +17,17 @@ class DatabaseConfig {
         }
     }
 
-    public final static String DRIVER_NAME = prop.getProperty("db.driver");
-    public final static String DB_HOST = prop.getProperty("db.host");
-    public final static String DB_PORT = prop.getProperty("db.port");
-    public final static String DB_NAME = prop.getProperty("db.name");
-    public final static String DB_USER_NAME = prop.getProperty("db.username");
-    public final static String DB_PASSWORD = prop.getProperty("db.password");
+    // Fetch from env variable first, if not found then use properties file
+    public final static String DRIVER_NAME = getEnvOrProperty("db.driver");
+    public final static String DB_HOST = getEnvOrProperty("db.host");
+    public final static String DB_PORT = getEnvOrProperty("db.port");
+    public final static String DB_NAME = getEnvOrProperty("db.name");
+    public final static String DB_USER_NAME = getEnvOrProperty("db.username");
+    public final static String DB_PASSWORD = getEnvOrProperty("db.password");
     public final static String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+
+    private static String getEnvOrProperty(String key) {
+        return System.getenv().getOrDefault(key.toUpperCase().replace(".", "_"), prop.getProperty(key));
+    }
 
 }
